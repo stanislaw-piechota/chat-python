30995504dd88756775e28b5b537ad77aa2b50220
Kacper <kacper.wadas17@gmail.com>
2022-06-05 23:23:59 +0200

    1.6

---
+++ globals.py
@@ -0,0 +1,9 @@
+def init():
+    global nickname
+    nickname = ''
+    global rooms
+    rooms = []
+    global room
+    room = ''
+    global m_id
+    m_id = 0
--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 import messages as ms
+import globals as gl
 from requests import get, post
 from requests.exceptions import HTTPError, ConnectionError, ConnectTimeout
 from time import sleep
@@ -21,7 +22,7 @@ def messages_thread(root):
 
 
 def get_messages():
-    messages = get(HOST+'chat.json').json()
+    messages = get(HOST+'chats.json').json()
     print(messages)
 
 
@@ -30,3 +31,40 @@ def ex_handle(func, *args):
         return func(*args)
     except (HTTPError, ConnectionError, ConnectTimeout) as e:
         print(f'Nie udało się pobrać wiadomości:  {e}')
+
+def anonymous():
+
+
+
+def register(username, password):
+    response = post(HOST, data={'register': True, 'username': username, 'password': password})
+    if 'Registered' in response.text:
+        pass
+    elif 'Error' in response.text:
+        print(response.json()["Error"])
+
+
+def log_in(username, password):
+    response = post(HOST, data={'login': True, 'username': username, 'password': password})
+    if 'Success' in response.text:
+        gl.rooms = response.json()["Success"]
+    elif 'Error' in response.text:
+        print(response.json()["Error"])
+    gl.rooms = response.json()
+
+
+def room_create(name):
+    response = get(HOST, params={'create': True, 'name': name})
+    if 'Success' in response.text:
+        room_join(gl.username, name)
+    elif 'Error' in response.text:
+        print(response.json()["Error"])
+
+
+def room_join(username, room):
+    response = get(HOST, params={'join': True, 'username': username, 'room': room})
+    if 'Success' in response.text:
+        gl.rooms.append(room)
+        gl.room = room
+    elif 'Error' in response.text:
+        print(response.json()["Error"])
